d0a6ce33de4480c855d7cc60d96ecb9e3a339e87
aysudlgl <112423731+aysudlgl@users.noreply.github.com>
2024-03-06 15:38:52 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {TodoWrapper} from "./components/todoWrapper";
+//import Greet from './components/greet'
+//import Welcome from "./components/welcome"
+//import Message from "./components/message"
+//import Hello from "./components/hello"
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+      {/*  <Greet name="Bruce" heroName={"Batman"}>
+            <p>This is children props</p>
+        </Greet>
+        <Greet name="Clark" heroName={"Superman"} >
+        <button>Action</button>
+        </Greet>
+        <Welcome name="Diana" heroName={"Wonder-woman"}>
+        </Welcome>
+        <Message> </Message>
+        <Hello></Hello>*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hello.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Hello = () => {
+    return React.createElement(
+        'div',{id:'hello', className:'dummyClass'},
+        React.createElement('h1',null,'Hello Aysu'))
+}
+export default Hello
